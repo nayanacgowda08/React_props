49aa375c88c535c1029ee1b2b81c0eea2b253bdf
nayanacgowda08 <132810494+nayanacgowda08@users.noreply.github.com>
2024-08-16 01:27:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Card from './assets/Components/Card'
 
 function App() {
   const data = [
-    {name:"nayna" , profession:"web developer" , image:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
-    {name:"raj" , profession:"app developer" , image:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false},
-    {name:"shishira" , profession:"nurse" , image:"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false},
-    {name:"manish" , profession:"painter" , image:"https://images.unsplash.com/photo-1504275490777-45f30792f13f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false}
+    {name:"name1" , profession:"web developer" , image:"https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"name2" , profession:"app developer" , image:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false},
+    {name:"name3" , profession:"nurse" , image:"https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false},
+    {name:"name4" , profession:"painter" , image:"https://images.unsplash.com/photo-1504275490777-45f30792f13f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHBvcnRyYWl0fGVufDB8fDB8fHww",friends:false}
   ] 
 
   const [val,setVal] = useState(data)
